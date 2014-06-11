12bafc975dee889dd6ec4057a0a379b9971cee48
Jezer Crespo <jezercrespo@gmail.com>
2014-06-11 18:51:02 +0800

    added broadcast functionality

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -5,12 +5,30 @@ require 'gcm'
 class MessageController < ApplicationController
   def send_push
 
-  	user = User.where("username='#{params["user"]}'").first;
+
   	sender = User.where("regid='#{params["sender"]}'").first;
 
+  	type=""
+  	user= Array.new
+  	if params['user'] == "All"
+  		User.all.each do |_user|
+  		type="broadcast"
+  		user.push(_user.regid)
+  		end
+
+  	else
+  		 singleReceiver = User.where("username='#{params["user"]}'").first;
+  		user.push(params['sender'])
+  		user.push(singleReceiver.regid)
+  		type = "whisper"
+  	end
+
+
+
+
 gcm = GCM.new("AIzaSyBJPHU6C4TYeUvcARkitaQRqzwshdavKU8")
-registration_ids= [user.regid]
-options = {data: {msg: "#{params['message']}", sender: "#{sender.username}"}, collapse_key: "new_message"}
+registration_ids= user
+options = {data: {msg: "#{params['message']}", sender: "#{sender.username}", type: "#{type}"}, collapse_key: "new_message"}
 response = gcm.send_notification(registration_ids, options)
 puts response
 
